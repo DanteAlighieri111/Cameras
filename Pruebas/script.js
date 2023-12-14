377f26ec3f2f78f2dce8b54052effbdeae1970e6
DanteAlighieri111 <148893118+DanteAlighieri111@users.noreply.github.com>
2023-12-14 11:19:18 -0500

    confirmacion

--- Pruebas/script.js
+++ Pruebas/script.js
@@ -1,23 +1,6 @@
-// Función para cargar la imagen con una función llamada cargarImagen
-function cargarImagen(imagenURL) {
-  // Redirige a la página externa y pasa la URL de la imagen como parámetro
-  window.location.href = 'pagina-externa.html?imagen=' + encodeURIComponent(imagenURL);
-}
-
-// Función para cargar otra imagen con una función llamada cargarOtraImagen
-function cargarOtraImagen(imagenURL) {
-  // Redirige a la página externa y pasa la URL de la imagen como parámetro
-  window.location.href = 'pagina-externa.html?imagen=' + encodeURIComponent(imagenURL);
-}
-
-// Obtén la URL de la imagen de los parámetros
-const imagenURL = obtenerParametroURL('imagen');
-
-if (imagenURL) {
-  // Crea un elemento de imagen y establece la fuente
-  const imagen = document.createElement('img');
-  imagen.src = imagenURL;
-
-  // Agrega la imagen al contenedor
-  document.getElementById('contenedor-imagen').appendChild(imagen);
+function toggleMenu(numero) {
+  const contenedor = document.getElementById(`contenedor${numero}`);
+  const menu = document.getElementById(`menu${numero}`);
+  menu.classList.toggle('mostrar-menu');
+  contenedor.classList.toggle('mostrar-menu');
 }
